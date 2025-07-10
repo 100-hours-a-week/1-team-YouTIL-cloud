b1fa02339a9efab0d2ead80719a586f8d30412b3
JM_DEV <kyungheeyj45@ajou.ac.kr>
2025-07-10 16:24:18 +0900

    feat: GKE VPC 모듈 추가

---
+++ v3-gcp/modules/vpc/variables.tf
@@ -0,0 +1,42 @@
+variable "vpc_name" {
+  description = "VPC network name"
+  type        = string
+  default     = "youtil-vpc-prod"
+}
+
+variable "region" {
+  description = "GCP 리전"
+  type        = string
+  default     = "asia-northeast2"
+}
+
+variable "bastion_subnet_cidr" {
+  description = "CIDR range for bastion subnet"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+
+variable "node_subnet_cidr" {
+  description = "CIDR range for node subnet"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "db_subnet_cidr" {
+  description = "CIDR range for db subnet"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "internal_cidr" {
+  description = "CIDR range for internal communication"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "ssh_source_ranges" {
+  description = "Source IP ranges allowed to SSH to bastion"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
